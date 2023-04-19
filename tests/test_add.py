93689ad5b3befc9705300f59b7dc984c3efd5f9c
Approaching-Truth <202109174@post.au.dk>
2023-04-19 13:35:41 +0200

    Test will succeed

--- tests/test_add.py
+++ tests/test_add.py
@@ -5,7 +5,7 @@ from addtest.add import add
 @pytest.mark.parametrize("x, y, result", [
     (2, 2, 4),
     (2, 2, 4),
-    (2, 2, 5),
+    (2, 2, 4),
 ])
 def test_addition(x, y, result):
     assert add(x, y) is result
